79fa5a2d1b4a685a248d8934dfe947e18e9dcf41
Michael Henke <smack42@googlemail.com>
2016-05-08 00:37:49 +0200

    read lines from pc19 file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+// RustFill - a Flood-It solver written in Rust
+// Copyright (C) 2016 Michael Henke
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
 fn main() {
-    println!("Hello, world!");
+    println!("RustFill");
+    match do_pc19() {
+        Ok(_)  => { println!("done.") },
+        Err(e) => { println!("error!"); println!("{}", e); std::process::exit(1); }
+    }
+}
+
+
+fn do_pc19() -> std::io::Result<String> {
+    let f = try!(File::open("pc19/tiles.txt"));
+    let reader = BufReader::new(f);
+    let mut num_lines = 0u32;
+    // read and count all lines from the file
+    for line in reader.lines() {
+        let line: String = match line {
+            Ok(s) => {
+                // stop processing when line has unexpected length
+                if s.len() != 14*14 { break; }
+                else { num_lines += 1; s }
+            },
+            // return the error immediately
+            Err(_) => { return line; }
+        };
+        println!("{:04} {}", num_lines, line);
+    }
+    println!("number of lines processed: {}", num_lines);
+    Ok(String::new())
 }
